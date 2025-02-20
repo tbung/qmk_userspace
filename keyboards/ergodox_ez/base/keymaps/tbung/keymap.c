3440c764cf8d1eb5953d0b305021b0ebb07203b6
Till Bungert <tillbungert@gmail.com>
2025-02-20 15:33:34 +0100

    chore: clean up symbols layer

--- keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
+++ keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
@@ -6,7 +6,7 @@
 enum layers {
     DEFAULT, // default layer
     SYMBOLS, // symbols
-    NAVIGAT, // media keys
+    NAV,     // mouse and other stuff
 };
 
 enum custom_keycodes {
@@ -65,20 +65,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [DEFAULT] = LAYOUT_ergodox_pretty(
-  KC_EQL,        KC_1,        KC_2,         KC_3,    KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,     KC_9,           KC_0,                 KC_MINS,
-  KC_DEL,        KC_Q,        KC_W,         KC_E,    KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,     KC_O,           KC_P,                 KC_BSLS,
-  GUI_T(KC_ESC), KC_A,        KC_S,         KC_D,    KC_F,    KC_G,                       KC_H, KC_J,  KC_K,     KC_L,           LT(NAVIGAT, KC_SCLN), GUI_T(KC_QUOT),
-  KC_LSFT,       CTL_T(KC_Z), LALT_T(KC_X), KC_C,    KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  KC_COMM,  RALT_T(KC_DOT), CTL_T(KC_SLSH),       KC_RSFT,
-  _______,       _______,     MO(SYMBOLS),  KC_LEFT, KC_RGHT,                                   KC_UP, KC_DOWN,  MO(SYMBOLS),    _______,              _______,
+  KC_EQL,        KC_1,        KC_2,         KC_3,    KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,     KC_9,           KC_0,             KC_MINS,
+  KC_DEL,        KC_Q,        KC_W,         KC_E,    KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,     KC_O,           KC_P,             KC_BSLS,
+  GUI_T(KC_ESC), KC_A,        KC_S,         KC_D,    KC_F,    KC_G,                       KC_H, KC_J,  KC_K,     KC_L,           LT(NAV, KC_SCLN), GUI_T(KC_QUOT),
+  KC_LSFT,       CTL_T(KC_Z), LALT_T(KC_X), KC_C,    KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  KC_COMM,  RALT_T(KC_DOT), CTL_T(KC_SLSH),   KC_RSFT,
+  _______,       _______,     MO(SYMBOLS),  KC_LEFT, KC_RGHT,                                   KC_UP, KC_DOWN,  MO(SYMBOLS),    _______,          _______,
                                                            _______, _______,     _______, _______,
                                                                     _______,     _______,
-                            LT(SYMBOLS, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, KC_TAB, LSFT_T(KC_ENT)
+                                LT(NAV, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, LSFT_T(KC_TAB), LT(SYMBOLS, KC_ENT)
 ),
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
- * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
+ * |---------+------+------+------+------+------+------|           |------+------+------+------+-----+------+--------|
  * |         |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
@@ -96,14 +96,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
 [SYMBOLS] = LAYOUT_ergodox_pretty(
-  _______, KC_F1,          KC_F2,           KC_F3,   KC_F4,   KC_F5,   _______,     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,        KC_F10,         KC_F11,
-  _______, KC_EXLM,        KC_AT,           KC_LCBR, KC_RCBR, KC_PIPE, _______,     _______, KC_UP,   KC_7,    KC_8,    KC_9,         KC_ASTR,        KC_F12,
-  _______, KC_HASH,        KC_DLR,          KC_LPRN, KC_RPRN, KC_GRV,                        KC_DOWN, KC_4,    KC_5,    KC_6,         KC_PLUS,        _______,
-  _______, CTL_T(KC_PERC), LALT_T(KC_CIRC), KC_LBRC, KC_RBRC, KC_TILD, _______,     _______, KC_AMPR, KC_1,    KC_2,    RALT_T(KC_3), CTL_T(KC_BSLS), _______,
-  _______, _______,        _______,         KC_UNDS, KC_MINS,                                         KC_DOT,  KC_0,    KC_EQL,       _______,        _______,
-                                                              _______, _______,     _______, _______,
-                                                                       _______,     _______,
-                                                     _______, _______, _______,     _______, _______, _______
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, _______,     _______, _______, _______, _______, _______, _______, KC_F12,
+  _______, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                        _______, _______, _______, _______, _______, _______,
+  _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, _______,     _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, KC_UNDS, KC_MINS,                                         _______, _______, _______, _______, _______,
+                                               _______, _______,     _______, _______,
+                                                        _______,     _______,
+                                      _______, _______, _______,     _______, _______, _______
 ),
 /* Keymap 2: Media and mouse keys
  *
@@ -126,16 +126,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-[NAVIGAT] = LAYOUT_ergodox_pretty(
+[NAV] = LAYOUT_ergodox_pretty(
   _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, KC_MS_U, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,                       KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, KC_MPLY,
-  _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, KC_MPRV, KC_MNXT, _______, _______,
-  _______, _______, _______, KC_BTN1, KC_BTN2,                                         KC_VOLU, KC_VOLD, KC_MUTE, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                       KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______,                                         _______, _______, _______, _______, _______,
 
                                                _______, _______,     _______, _______,
                                                         _______,     _______,
-                                      _______, _______, _______,     _______, _______, KC_WBAK
+                                      _______, _______, _______,     _______, _______, _______
 ),
 };
 // clang-format on
