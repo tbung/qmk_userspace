e1c328d986af14c9f155f39223d620ab11477610
Till Bungert <tillbungert@gmail.com>
2025-02-20 23:08:43 +0100

    fix: no pinky shift is unbearable

--- keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
+++ keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
@@ -65,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [DEFAULT] = LAYOUT_ergodox_pretty(
-  KC_EQL,  KC_1,        KC_2,         KC_3,    KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,     KC_9,           KC_0,           KC_MINS,
-  KC_DEL,  KC_Q,        KC_W,         KC_E,    KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,     KC_O,           KC_P,           KC_BSLS,
-  KC_ESC,  KC_A,        KC_S,         KC_D,    KC_F,    KC_G,                       KC_H, KC_J,  KC_K,     KC_L,           KC_SCLN,        KC_QUOT,
-  KC_LGUI, CTL_T(KC_Z), LALT_T(KC_X), KC_C,    KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  KC_COMM,  RALT_T(KC_DOT), CTL_T(KC_SLSH), KC_RGUI,
-  _______, _______,     _______,      KC_LEFT, KC_RGHT,                                   KC_UP, KC_DOWN,  _______,        _______,        _______,
-                                                     _______, _______,     _______, _______,
-                                                              _______,     _______,
-                          LT(NAV, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, LSFT_T(KC_TAB), LT(SYMBOLS, KC_ENT)
+  KC_EQL,  KC_1,        KC_2,    KC_3,           KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,            KC_9,    KC_0,           KC_MINS,
+  KC_DEL,  KC_Q,        KC_W,    KC_E,           KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,            KC_O,    KC_P,           KC_BSLS,
+  KC_ESC,  KC_A,        KC_S,    KC_D,           KC_F,    KC_G,                       KC_H, KC_J,  KC_K,            KC_L,    KC_SCLN,        KC_QUOT,
+  KC_LSFT, CTL_T(KC_Z), KC_X,    KC_C,           KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  KC_COMM,         KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
+  _______, _______,     KC_LGUI, ALT_T(KC_LEFT), KC_RGHT,                                   KC_UP, RALT_T(KC_DOWN), KC_RGUI, _______,        _______,
+                                                       _______, _______,     _______, _______,
+                                                                _______,     _______,
+                            LT(NAV, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, LSFT_T(KC_TAB), LT(SYMBOLS, KC_ENT)
 ),
 /* Keymap 1: Symbol Layer
  *
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SYMBOLS] = LAYOUT_ergodox_pretty(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,     _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, KC_EXLM, KC_LCBR, KC_RCBR, KC_AT,   KC_PIPE, _______,     _______, _______, _______, _______, _______, _______, KC_F12,
+  _______, KC_EXLM, KC_LCBR, KC_RCBR, KC_AT,   KC_PERC, _______,     _______, _______, _______, _______, _______, _______, KC_F12,
   KC_ASTR, KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL,  KC_GRV,                        _______, _______, _______, _______, _______, _______,
   KC_HASH, KC_AMPR, KC_LBRC, KC_RBRC, KC_CIRC, KC_TILD, _______,     _______, _______, _______, _______, _______, _______, _______,
   _______, _______, KC_UNDS, KC_MINS, KC_DLR,                                          _______, _______, _______, _______, _______,
