395c323b53deb68248ed80b95f64e0eff4695e6e
Till Bungert <tillbungert@gmail.com>
2025-02-20 23:47:25 +0100

    feat: make shift sticky

--- keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
+++ keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
@@ -65,14 +65,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [DEFAULT] = LAYOUT_ergodox_pretty(
-  KC_EQL,  KC_1,        KC_2,    KC_3,           KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,            KC_9,    KC_0,           KC_MINS,
-  KC_DEL,  KC_Q,        KC_W,    KC_E,           KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,            KC_O,    KC_P,           KC_BSLS,
-  KC_ESC,  KC_A,        KC_S,    KC_D,           KC_F,    KC_G,                       KC_H, KC_J,  KC_K,            KC_L,    KC_SCLN,        KC_QUOT,
-  KC_LSFT, CTL_T(KC_Z), KC_X,    KC_C,           KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  KC_COMM,         KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
-  _______, _______,     KC_LGUI, ALT_T(KC_LEFT), KC_RGHT,                                   KC_UP, RALT_T(KC_DOWN), KC_RGUI, _______,        _______,
-                                                       _______, _______,     _______, _______,
-                                                                _______,     _______,
-                            LT(NAV, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, LSFT_T(KC_TAB), LT(SYMBOLS, KC_ENT)
+  KC_EQL,        KC_1,        KC_2,    KC_3,           KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,            KC_9,    KC_0,           KC_MINS,
+  KC_DEL,        KC_Q,        KC_W,    KC_E,           KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,            KC_O,    KC_P,           KC_BSLS,
+  KC_ESC,        KC_A,        KC_S,    KC_D,           KC_F,    KC_G,                       KC_H, KC_J,  KC_K,            KC_L,    KC_SCLN,        KC_QUOT,
+  OSM(MOD_LSFT), CTL_T(KC_Z), KC_X,    KC_C,           KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  KC_COMM,         KC_DOT,  CTL_T(KC_SLSH), OSM(MOD_RSFT),
+  _______,       _______,     KC_LGUI, ALT_T(KC_LEFT), KC_RGHT,                                   KC_UP, RALT_T(KC_DOWN), KC_RGUI, _______,        _______,
+                                                             _______, _______,     _______, _______,
+                                                                      _______,     _______,
+                                  LT(NAV, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, LSFT_T(KC_TAB), LT(SYMBOLS, KC_ENT)
 ),
 /* Keymap 1: Symbol Layer
  *
