e6c2ef64fd70718e8fc5fe02ad5a1da8465b088b
Till Bungert <tillbungert@gmail.com>
2025-02-21 23:56:23 +0100

    fix: remove dedicated cmd key

--- keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
+++ keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
@@ -69,14 +69,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [DEFAULT] = LAYOUT_ergodox_pretty(
-  KC_EQL,        KC_1,         KC_2,         KC_3,    KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,    KC_9,           KC_0,            KC_MINS,
-  KC_DEL,        KC_Q,         KC_W,         KC_E,    KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,    KC_O,           KC_P,            KC_BSLS,
-  KC_ESC,        KC_A,         KC_S,         KC_D,    KC_F,    KC_G,                       KC_H, KC_J,  KC_K,    KC_L,           KC_SCLN,         KC_QUOT,
-  OSM(MOD_LSFT), LCTL_T(KC_Z), LALT_T(KC_X), KC_C,    KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  KC_COMM, RALT_T(KC_DOT), RCTL_T(KC_SLSH), OSM(MOD_RSFT),
-  _______,       _______,      KC_LCMD,      KC_LEFT, KC_RGHT,                                   KC_UP, KC_DOWN, KC_RCMD,        _______,         _______,
-                                                            _______, _______,     _______, _______,
-                                                                     _______,     _______,
-                                 LT(NAV, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, LSFT_T(KC_TAB), LT(SYMBOLS, KC_ENT)
+  KC_EQL,        KC_1,         KC_2,         KC_3,         KC_4,    KC_5, KC_MUTE,     KC_MPLY, KC_6, KC_7,  KC_8,            KC_9,           KC_0,            KC_MINS,
+  KC_DEL,        KC_Q,         KC_W,         KC_E,         KC_R,    KC_T, KC_VOLU,     MS_WHLU, KC_Y, KC_U,  KC_I,            KC_O,           KC_P,            KC_BSLS,
+  KC_ESC,        KC_A,         KC_S,         KC_D,         KC_F,    KC_G,                       KC_H, KC_J,  KC_K,            KC_L,           KC_SCLN,         KC_QUOT,
+  OSM(MOD_LSFT), LCTL_T(KC_Z), LALT_T(KC_X), LGUI_T(KC_C), KC_V,    KC_B, KC_VOLD,     MS_WHLD, KC_N, KC_M,  RGUI_T(KC_COMM), RALT_T(KC_DOT), RCTL_T(KC_SLSH), OSM(MOD_RSFT),
+  _______,       _______,      _______,      KC_LEFT,      KC_RGHT,                                   KC_UP, KC_DOWN,         _______,        _______,         _______,
+                                                                 _______, _______,     _______, _______,
+                                                                          _______,     _______,
+                                      LT(NAV, KC_SPC), TD(ADAPTIVE_BSPC), _______,     _______, LSFT_T(KC_TAB), LT(SYMBOLS, KC_ENT)
 ),
 /* Keymap 1: Symbol Layer
  *
