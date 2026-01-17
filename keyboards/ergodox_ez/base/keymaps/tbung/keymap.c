13e0ad6e1a381c098709f0d3ae9dffba65587c8e
Till Bungert <tillbungert@gmail.com>
2026-01-18 00:43:24 +0100

    feat: just make keys universal on os side

--- keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
+++ keyboards/ergodox_ez/base/keymaps/tbung/keymap.c
@@ -16,15 +16,6 @@ enum layers {
     SYSTEM,
 };
 
-enum custom_keycodes {
-    VRSN = SAFE_RANGE,
-    C_UNDO,
-    C_CUT,
-    C_COPY,
-    C_PASTE,
-    C_BSPC,
-};
-
 tap_dance_action_t tap_dance_actions[] = {
     [TD_KC_BSPC] = ACTION_TAP_DANCE_FN_ADVANCED_WITH_RELEASE(NULL, td_bspc_on_each_release, td_bspc_on_dance_finished, td_bspc_on_dance_reset),
 };
@@ -91,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, KC_AT,   KC_AMPR, KC_DLR,                                          _______, _______, _______, _______, _______,
                                                _______, _______,     _______, _______,
                                                         _______,     _______,
-                                      _______,  C_BSPC, _______,     _______, _______, _______
+                               _______,  HYPR(KC_BSPC), _______,     _______, _______, _______
 ),
 /* Keymap 2: Media and mouse keys
  *
@@ -118,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,    KC_F1,      KC_F2,        KC_F3,      KC_F4,      KC_F5,      _______,     _______, KC_F6,   KC_F7,      KC_F8,   KC_F9,      KC_F10,  KC_F11,
   _______,    HYPR(KC_1), HYPR(KC_2),   HYPR(KC_3), HYPR(KC_4), HYPR(KC_5), _______,     _______, _______, RALT(KC_U), _______, RALT(KC_O), _______, KC_F12,
   _______,    RALT(KC_A), RALT(KC_S),   _______,    _______,    _______,                          MS_LEFT, MS_DOWN,    MS_UP,   MS_RGHT,    _______, _______,
-  MO(SYSTEM), LCTL(KC_Z), LSFT(KC_DEL), C_COPY,     C_PASTE,    _______,    _______,     _______, _______, _______,    _______, _______,    _______, _______,
+  MO(SYSTEM), KC_UNDO, KC_CUT, KC_COPY,     KC_PASTE,    _______,    _______,     _______, _______, _______,    _______, _______,    _______, _______,
   _______,    _______,    _______,      HYPR(KC_LEFT),    HYPR(KC_RIGHT),                                               HYPR(KC_UP),    HYPR(KC_DOWN), _______,    _______, _______,
                                                                    _______, _______,     _______, _______,
                                                                             _______,     _______,
@@ -137,87 +128,6 @@ _______, _______, QK_BOOTLOADER, QK_REBOOT, _______,                            
 ),
 };
 // clang-format on
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-#ifdef CONSOLE_ENABLE
-    const bool is_combo = record->event.type == COMBO_EVENT;
-    uprintf("0x%04X,%u,%u,%u,%b,0x%02X,0x%02X,%u\n", keycode, is_combo ? 254 : record->event.key.row, is_combo ? 254 : record->event.key.col, get_highest_layer(layer_state), record->event.pressed, get_mods(), get_oneshot_mods(), record->tap.count);
-#endif
-    // tap_dance_action_t *action;
-    os_variant_t os = detected_host_os();
-
-    if (record->event.pressed) {
-        switch (keycode) {
-            case VRSN:
-                SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-                return false;
-            case C_UNDO:
-                if (os == OS_MACOS || os == OS_IOS)
-                    register_code16(LCMD(KC_Z));
-                else
-                    register_code16(LCTL(KC_Z));
-                return false;
-            case C_CUT:
-                if (os == OS_MACOS || os == OS_IOS)
-                    register_code16(LCMD(KC_X));
-                else
-                    register_code16(LSFT(KC_DEL));
-                return false;
-            case C_COPY:
-                if (os == OS_MACOS || os == OS_IOS)
-                    register_code16(LCMD(KC_C));
-                else
-                    register_code16(LCTL(KC_INS));
-                return false;
-            case C_PASTE:
-                if (os == OS_MACOS || os == OS_IOS)
-                    register_code16(LCMD(KC_V));
-                else
-                    register_code16(LSFT(KC_INS));
-                return false;
-            case C_BSPC:
-                if (os == OS_MACOS || os == OS_IOS)
-                    register_code16(LALT(KC_BSPC));
-                else
-                    register_code16(LCTL(KC_BSPC));
-                return false;
-        }
-    } else {
-        switch (keycode) {
-            case C_UNDO:
-                if (os == OS_MACOS || os == OS_IOS)
-                    unregister_code16(LCMD(KC_Z));
-                else
-                    unregister_code16(LCTL(KC_Z));
-                return false;
-            case C_CUT:
-                if (os == OS_MACOS || os == OS_IOS)
-                    unregister_code16(LCMD(KC_X));
-                else
-                    unregister_code16(LSFT(KC_DEL));
-                return false;
-            case C_COPY:
-                if (os == OS_MACOS || os == OS_IOS)
-                    unregister_code16(LCMD(KC_C));
-                else
-                    unregister_code16(LCTL(KC_INS));
-                return false;
-            case C_PASTE:
-                if (os == OS_MACOS || os == OS_IOS)
-                    unregister_code16(LCMD(KC_V));
-                else
-                    unregister_code16(LSFT(KC_INS));
-                return false;
-            case C_BSPC:
-                if (os == OS_MACOS || os == OS_IOS)
-                    unregister_code16(LALT(KC_BSPC));
-                else
-                    unregister_code16(LCTL(KC_BSPC));
-                return false;
-        }
-    }
-    return true;
-}
 
 // Runs just one time when the keyboard initializes.
 void keyboard_post_init_user(void) {};
